eaf608c6f72c232c0dd6bef458b98f4019569610
Nathan Bittner <njbittner@gmail.com>
2023-02-10 02:30:36 -0800

    fix cnn large

--- bsuite_utils/mnist_wrapper.py
+++ bsuite_utils/mnist_wrapper.py
@@ -78,11 +78,11 @@ class SmallCNN(BaseFeaturesExtractor):
         )
         elif scale == 'large':
             self.cnn = nn.Sequential(
-                nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4),
+                nn.Conv2d(n_input_channels, 32, kernel_size=5, padding='same'),
                 nn.ReLU(),
-                nn.Conv2d(32, 64, kernel_size=4, stride=2),
+                nn.Conv2d(32, 64, kernel_size=5, padding='same'),
                 nn.ReLU(),
-                nn.Conv2d(64, 64, kernel_size=3, stride=1),
+                nn.Conv2d(64, 64, kernel_size=5, padding='same'),
                 nn.Flatten(),
             )
 
